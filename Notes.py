ac528d21364abbc9f3504120fea71198361e75ea
RosieRivera57355 <33767543+RosieRivera57355@users.noreply.github.com>
2017-12-04 15:39:03 -0800

    Added Stuff

--- Notes.py
+++ Notes.py
@@ -120,3 +120,29 @@ print("Hello \nWorld")
 
 
 print(random.randint(0, 6))
+#
+#
+# def reverse_order(first_name, last_name):
+#     print("%s %s" % (last_name, first_name))
+#
+#
+# def reverse_order():
+#     first_name = input("What is your first name?")
+#     last_name = input("What is your last name?")
+#     print("%s %s" % (last_name, first_name))
+
+"""Warmup #2
+Write a function called "happy_bday"
+that "sings" (prints)
+the Happy Birthday Song
+
+It must take one parameter called "name" """
+
+
+def happy_bday(name):
+    print("Happy Birthday to you")
+    print("Happy Birthday to you")
+    print("Happy Birthday to " + name)
+    print("Happy Birthday to you!!!")
+
+
