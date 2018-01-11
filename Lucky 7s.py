432289911c1d1f926610e8abd0add10d2e6571e7
Rosie Rivera <33767543+RosieRivera57355@users.noreply.github.com>
2018-01-11 15:39:22 -0800

    Added Stuff

--- Lucky 7s.py
+++ Lucky 7s.py
@@ -3,10 +3,20 @@ import random
 die_num = random.randint(1, 6)
 die_num2 = random.randint(1, 6)
 m = 15
-
-def say_hi(roll):
-    print("Hello %s." % roll)
-
-
 dice = die_num + die_num2
-for
+
+print("You have %s." % m)
+
+while m > 0:
+    answer = int(input("Type in roll to Roll the Dice."))
+    if dice == 7:
+        print("Good job!Now you have %s Money" % m)
+        m += 5
+    elif dice < 7:
+        m -= 1
+        print("I snatched your money now you have %s money" % m)
+    elif dice > 7:
+        m -= 1
+        print("I snatched your money now you have %s money" % m)
+    if m is 0:
+        print("You have no moeny! GET OUT!")
