28e0ca587e42f4735509c99197878d119e19fe21
Rosie Rivera <33767543+RosieRivera57355@users.noreply.github.com>
2018-01-17 15:39:29 -0800

    Added Stuff

--- Black Jack.py
+++ Black Jack.py
@@ -1,2 +1,12 @@
 import random
 
+card = 0
+
+print("Hi loser, my name is Natasha. What's yours...? e-e")
+
+    card_num = random.randint(1, 11)
+
+    if card > 21:
+        print("You Bust! O3O")
+    if card == 21:
+        print("You win... I guess e-e")
