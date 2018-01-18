ae1b534eccd63555e93d42f2a21a699c26dbf430
Rosie Rivera <33767543+RosieRivera57355@users.noreply.github.com>
2018-01-18 15:36:52 -0800

    Added Stuff

--- Notes.py
+++ Notes.py
@@ -48,78 +48,78 @@ import random  # imports should be at the top of your program
 #
 # print("You are %s?" % age)
 
-
-def print_hw():
-    print("Hello World")
-
-
-print_hw()
-
-
-def say_hi(name):  # name is a "parameter"
-    print("Hello %s." % name)
-    print("I hope you have a fantastic day!")
-
-
-say_hi("Johnny")
-
-
-def birthday(age):
-    age += 1  # age = age + 1
-    print(age)
-
-
-say_hi("John")
-print("John is 15. Next Year:")
-birthday(15)
-
-
-def f(x):
-    return x ** 5 + 4 * x ** 4 - 17 * x ** 2 + 4
-
-
-print(f(3))
-print(f(3) + f(5))
-
-# If statements
-
-
-def grade_calc(percentage):
-    if percentage >= 90:
-        return "A"
-    elif percentage >= 80:  # Else if statement
-        return "B"
-    elif percentage >= 70:
-        return "C"
-    elif percentage >= 60:
-        return "D"
-    elif percentage <= 60:
-        return "F"
-
-# Loops
-
-
-print(range(5))
-
-for num in range(5):
-    print(num + 1)
-
-for letter in "Hello World":
-    print(letter)
-
-a = 1
-while a < 10:
-    print(a)
-    a += 1
-
-response = ""
-while response != "Hello":
-    response = input("Say\"Hello\"")
-
-print("Hello \nWorld")
-
-
-print(random.randint(0, 6))
+#
+# def print_hw():
+#     print("Hello World")
+#
+#
+# print_hw()
+#
+#
+# def say_hi(name):  # name is a "parameter"
+#     print("Hello %s." % name)
+#     print("I hope you have a fantastic day!")
+#
+#
+# say_hi("Johnny")
+#
+#
+# def birthday(age):
+#     age += 1  # age = age + 1
+#     print(age)
+#
+#
+# say_hi("John")
+# print("John is 15. Next Year:")
+# birthday(15)
+#
+#
+# def f(x):
+#     return x ** 5 + 4 * x ** 4 - 17 * x ** 2 + 4
+#
+#
+# print(f(3))
+# print(f(3) + f(5))
+#
+# # If statements
+#
+#
+# def grade_calc(percentage):
+#     if percentage >= 90:
+#         return "A"
+#     elif percentage >= 80:  # Else if statement
+#         return "B"
+#     elif percentage >= 70:
+#         return "C"
+#     elif percentage >= 60:
+#         return "D"
+#     elif percentage <= 60:
+#         return "F"
+#
+# # Loops
+#
+#
+# print(range(5))
+#
+# for num in range(5):
+#     print(num + 1)
+#
+# for letter in "Hello World":
+#     print(letter)
+#
+# a = 1
+# while a < 10:
+#     print(a)
+#     a += 1
+#
+# response = ""
+# while response != "Hello":
+#     response = input("Say\"Hello\"")
+#
+# print("Hello \nWorld")
+#
+#
+# print(random.randint(0, 6))
 #
 #
 # def reverse_order(first_name, last_name):
@@ -132,24 +132,56 @@ print(random.randint(0, 6))
 #     print("%s %s" % (last_name, first_name))
 
 # Comparisons
+#
+# print(1 == 1)  # Two equal signs to compare
+#
+# print(1 != 2)
+# # One is not equal to 2
+#
+# print(not False)  # This prints True
 
-print(1 == 1)  # Two equal signs to compare
+#
+# print(1 == 1 and 5 <= 4)
+#
+# print(1 < 0 or 5 > 1)
+#
+# # Recasting
+#
+# c = '1'
+#
+# print(c == 1)  # False - C is a string, 1 is an integer
+#
+# print(int(c) == 1)  # Compares two ints
+#
+# print(c == str(1))  # Compares two strings
 
-print(1 != 2)
-# One is not equal to 2
+# Lists
 
-print(not False)  # This prints True
+the_count = [1, 2, 3, 4, 5]
+characters = ["Graves", "Dory", "Boots", "Dora", "Shrek", "Obi-wan", "Carl"]
+print(characters[4])
 
-print(1 == 1 and 5 <= 4)
+print(len(characters))  # Gives you the length of the list
 
-print(1 < 0 or 5 > 1)
+# Going through lists
+for char in characters:
+    print(char)
 
-# Recasting
+for num in the_count:
+    print(num ** 2)
 
-c = '1'
+len(characters)
+range(3)  # Makes a list of the numbers from 0 to 2
+range(len(characters))  # Makes a list of ALL INDICES
 
-print(c == 1)  # False - C is a string, 1 is an integer
+for num in range(len(characters)):
+    char = characters[num]
+    print("The character at index %d is %s" % (num, char))
 
-print(int(c) == 1)  # Compares two ints
-
-print(c == str(1))  # Compares two strings
+str1 = "Hello World!"
+listOne = list(str1)
+print(listOne)
+listOne[11] = '.'
+print(listOne)
+newStr = "".join(listOne)
+print(newStr)
