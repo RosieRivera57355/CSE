04328d974373b9440da6a66e452a312e376950db
Rosie Rivera <33767543+RosieRivera57355@users.noreply.github.com>
2018-01-23 15:39:00 -0800

    Added Stuff

--- Rosie Rivera - Hangman.py
+++ Rosie Rivera - Hangman.py
@@ -4,13 +4,14 @@ import random
 Outline of Hangman
 1. Make a word bank - 10 items
 2. Select a random item from the list
-3. Hide the word (Use *)
-4. Reveal the letters if they have been guessed
+3. Add user input to a list of letters_guessed
+4. Build a list of letters to show, and display the string form
 5. Create the win condition
 
 """
+list_form = []
+guesses_left = 10
+letters_guessed = [""]
 word_bank = ["Click", "Robot", "School", "Batman", "Stitch", "Jack Black", "Shrek", "Luis", "50 Cent", "Beyonce"]
 word = random.choice(word_bank)
-print(word)
-print(word_bank.punctuation)
-print(punctuation)
+word_bank[word] = '*'
