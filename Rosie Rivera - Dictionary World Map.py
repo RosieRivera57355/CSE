31544a1d09b5b7e783e3b307da73f20a870d9bba
Rosie Rivera <33767543+RosieRivera57355@users.noreply.github.com>
2018-02-14 15:38:27 -0800

    added Stuff

--- Rosie Rivera - Dictionary World Map.py
+++ Rosie Rivera - Dictionary World Map.py
@@ -1,18 +1,17 @@
-world_map ={
+world_map = {
     'F-GATE': {
         'NAME': 'FRONT GATE',
-        'DESCRIPTION': 'The fence seems to continue south and it is locked. It seems you must find another way in so'
-        'that you will not get caught',
+        'DESCRIPTION': 'The fence seems to continue south and it is locked.\n It seems you must find another way in so'
+        ' that you will not get caught',
         'PATHS': {
-            'WEST': 'Parking Lot',
-            'SOUTH': 'Back Fence'
+            'SOUTH': 'B-FENCE'
         }
     },
     'B-FENCE': {
         'NAME': 'BROKEN FENCE',
         'DESCRIPTION': 'The Fence is loose and there is more of the fence to the east',
         'PATHS': {
-            'NORTH': 'FRONT GATE',
+            'NORTH': 'F-GATE',
             'NORTHWEST': 'GARDEN',
             'EAST': 'SEWER LID'
         }
@@ -22,7 +21,7 @@ world_map ={
         'DESCRIPTION': 'There is a path leading Northeast somewhere and there is a broken fence south of here.',
         'PATHS': {
             'SOUTHEAST': 'B-FENCE',
-            'NORTH': 'SOUTH OF HOUSE',
+            'NORTH': 'S-HOUSE',
             'NORTHEAST': 'SHED'
         }
     },
@@ -34,7 +33,20 @@ world_map ={
         }
     }
 }
-current_node = world_map['SOUTHHOUSE']
-print(current_node['DESCRIPTION'])
-print(current_node['NAME'])
+current_node = world_map['F-GATE']
+directions = ['NORTH', 'SOUTH', 'EAST', 'WEST', 'NORTHEAST', 'NORTHWEST', 'SOUTHEAST', 'SOUTHWEST', 'UP', 'DOWN']
 
+while True:
+    print(current_node["NAME"])
+    print(current_node['DESCRIPTION'])
+    command = input('>_')
+    if command in directions:
+        try:
+            name_of_node = current_node['PATHS'][command]
+            current_node = world_map[name_of_node]
+        except KeyError:
+            print("You cannot go that way.")
+    else:
+        print("Command not Found")
+    if command == 'quit':
+        quit(0)
