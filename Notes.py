6e4ad2848fcf11a79e60401696cd5ea673cc1cce
Rosie Rivera <33767543+RosieRivera57355@users.noreply.github.com>
2018-02-21 15:43:24 -0800

    Added Stuff

--- Notes.py
+++ Notes.py
@@ -315,8 +315,82 @@ import random  # imports should be at the top of your program
 
 # print(distance(0, 0, 3, 4))
 
+#
+# def pythagorean(a, b):
+#     answer = (a ** 2 + b ** 2)
+#
+# print(pythagorean(5, 12)
 
-def pythagorean(a, b):
-    answer ** 2 + b ** 2)
+# Defining a class
 
-print(pythagorean(5, 12)
+
+class Cat(object):
+    # Two underscores before and after
+
+    def __init__(self, color, pattern):
+        # Things that a Cat has
+        self.color = color
+        self.pattern = pattern
+        self.state = "happy"
+        self.hungry = False
+
+# Things that a Cat can do
+    def jump(self):
+        self.state = "Scared"
+        print("The cat jumps in the air")
+
+    def play(self):
+        self.state = "Happy"
+        print("You play with the cat")
+
+
+#  Instantiating (creating) two cats
+cute_cat = Cat("brown", "spots")
+cute_cat2 = Cat("gray", "no spots")
+
+#  Getting information about the cats
+print(cute_cat.color)
+print(cute_cat2.state)
+print(cute_cat2.color)
+
+cute_cat.jump()
+print(cute_cat.state)
+print(cute_cat2.state)
+
+cute_cat.play()
+print(cute_cat.state)
+
+
+class Car(object):
+    def __init__(self, color, brand, num_of_cylinders):
+        self.color = color
+        self.brand = brand
+        self.cylinders = num_of_cylinders
+        self.engineOn = False
+        self.engineOff = False
+
+    def turn_on(self):
+        if self. engineOn:
+            print("Nothing Happens")
+        else:
+            print("The engine turns on")
+            self.engineOn = True
+
+    def move_forward(self):
+        if self.engineOn:
+            print("You move forward")
+        else:
+            print("Nothing Happens")
+
+    def turn_off(self):
+        if self. engineOn:
+            self.engineOn = False
+            print("The engine turns off")
+        else:
+            print("Nothing happens")
+
+my_car = Car("blue", "Subaru", 4)
+
+my_car.turn_on()
+my_car.move_forward()
+my_car.turn_off()
