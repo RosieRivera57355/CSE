b251c0d46fb62fd4cbd1759381e9f8354208fbdc
Rosie Rivera <33767543+RosieRivera57355@users.noreply.github.com>
2018-04-09 15:38:47 -0700

    Added Stuff

--- Rosie Rivera - CYOA.py
+++ Rosie Rivera - CYOA.py
@@ -9,37 +9,73 @@
 
 
 class Item(object):
-    def __init__(self, name, use, description):
+    def __init__(self, name, use):
         self.name = name
         self.use = use
-        self.description = description
+
+    def drop(self):
+        print('You dropped %s' % self.name)
+
+    def pick_up(self):
+        print('You picked up %s' % self.name)
 
 
 class House(Item):
-    def __init__(self, name, use, description):
-        super(House, self).__init__(name, use, description)
+    def __init__(self, name, use):
+        super(House, self).__init__(name, 'open')
+
+    def open(self):
+        print('You opened the %s' % self.name)
 
 
 class Window(House):
-    def __init__(self, open):
+    def __init__(self):
         super(Window, self).__init__('window', 'open', 'The window is partly open')
 
-    def open(self):
+    def use(self):
         print('You opened the window.')
 
 
-class Fence(House):
-    def __init__(self, name, use, description):
-        super(House, self).__init__(name ,use , description)
-
-    def description(self):
-        print('There is a loose board.')
-
-    def name(self):
-        print('Fence')
+class Tools(Item):
+    def __init__(self, name, use):
+        super(Tools, self).__init__(name, use)
 
     def use(self):
-        print('You opened the %s' % self.name)
+        print('You used %s' % self.name)
+
+
+class Knife(Tools):
+    def __init__(self):
+        super(Knife, self).__init__('Knife', 'cut')
+
+    def name(self):
+        print('knife')
+
+    def cut(self):
+        print('You cut the potatoes.')
+
+
+class Furniture(Item):
+    def __init__(self, name, use):
+        super(Furniture, self).__init__(name, use)
+
+
+class Fridge(Furniture):
+    def __init__(self):
+        super(Fridge, self).__init__('Fridge', 'open')
+
+    def open(self):
+        print('There are some frozen chicken nuggets.')
+
+
+class Kitchen(Item):
+    def __init__(self, name, use):
+        super(Kitchen, self).__init__(name, use)
+
+
+class Misc(Item):
+    def __init__(self, name, use):
+        super(Misc, self).__init__(name, use)
 
 
 class Tools(Item):
@@ -47,36 +83,6 @@ class Tools(Item):
         super(Tools, self).__init__(name, use, description)
 
 
-class Knife(Tools):
-    def __init__(self, name, use, description):
-        super(Knife, self).__init__('Knife', 'cut', 'The knife looks like it can cut through many things.')
-
-    def name(self):
-        print('knife')
-
-    def use(self):
-        print('')
-
 class Key(Tools):
-    def __init__(self, name, use, description):
-        super(Key, self).__init__('Key', 'open', 'You can open something with this key.')
-
-
-class Furniture(Item):
-    def __init__(self, name, use, description):
-        super(Furniture, self).__init__(name, use, description)
-
-
-class Fridge(Furniture):
-    def __init__(self, name, use, description):
-        super(Fridge, self).__init__('Fridge', 'open', 'There are vegetables in here.')
-
-
-class Kitchen(Item):
-    def __init__(self, name, use, description):
-        super(Kitchen, self).__init__(name, use, description)
-
-
-class Misc(Item):
-    def __init__(self, name, use, description):
-        super(Misc, self).__init__(name, use, description)
+    def __init__(self):
+        super(Key, self).__init__('Key', 'open')
