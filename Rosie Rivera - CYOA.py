9335ff67ca49d7f2f349707a5485db3b726ab0fc
Rosie Rivera <33767543+RosieRivera57355@users.noreply.github.com>
2018-04-10 15:34:02 -0700

    Added Stuff

--- Rosie Rivera - CYOA.py
+++ Rosie Rivera - CYOA.py
@@ -20,19 +20,21 @@ class Item(object):
         print('You picked up %s' % self.name)
 
 
-class House(Item):
+class House(Item, open):
     def __init__(self, name, use):
         super(House, self).__init__(name, 'open')
+        self.open = open
 
     def open(self):
         print('You opened the %s' % self.name)
 
 
 class Window(House):
-    def __init__(self):
-        super(Window, self).__init__('window', 'open', 'The window is partly open')
+    def __init__(self, open):
+        super(Window, self).__init__('window', 'open')
+        self.open = open
 
-    def use(self):
+    def open(self):
         print('You opened the window.')
 
 
@@ -42,17 +44,6 @@ class Tools(Item):
 
     def use(self):
         print('You used %s' % self.name)
-
-
-class Knife(Tools):
-    def __init__(self):
-        super(Knife, self).__init__('Knife', 'cut')
-
-    def name(self):
-        print('knife')
-
-    def cut(self):
-        print('You cut the potatoes.')
 
 
 class Furniture(Item):
@@ -78,11 +69,20 @@ class Misc(Item):
         super(Misc, self).__init__(name, use)
 
 
-class Tools(Item):
-    def __init__(self, name, use, description):
-        super(Tools, self).__init__(name, use, description)
+class Helper(Item):
+    def __init__(self, name, use):
+        super(Helper, self).__init__(name, use)
 
 
-class Key(Tools):
+class Knife(Helper):
+    def __init__(self, cut):
+        super(Knife, self).__init__('Knife', 'cut')
+        self.cut = cut
+
+    def cut(self):
+        print('You cut the potatoes.')
+
+
+class Key(Helper):
     def __init__(self):
         super(Key, self).__init__('Key', 'open')
