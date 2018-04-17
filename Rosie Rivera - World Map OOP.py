f98f4e97f943b1d31796d40f974f5b20684fff0b
Rosie Rivera <33767543+RosieRivera57355@users.noreply.github.com>
2018-04-17 15:01:36 -0700

    Added Stuff

--- Rosie Rivera - World Map OOP.py
+++ Rosie Rivera - World Map OOP.py
@@ -1,23 +1,22 @@
 class Room(object):
-    def __int__(self, name, north, south, west, east, northeast, northwest, southeast, southwest, up, down, description, paths):
+    def __int__(self, name, north, south, west, east, northwest, northeast, southwest, southeast, up, down, description):
         self.name = name
         self.north = north
         self.south = south
         self.west = west
         self.east = east
-        self.northeast = northeast
         self.northwest = northwest
-        self.southeast = southeast
+        self.northeast = northeast
         self.southwest = southwest
+        self.southeast = southeast
         self.up = up
         self.down = down
         self.description = description
-        self.paths = paths
 
     def move(self, direction):
         global current_node
         current_node = globals()[getattr(self, direction)]
 
 
-fence = Room('West of House', 'north_house')
-west_house = Room('North of House', None)
+f_gate = Room('Front Gate', None, 'Fence', None, None, None, None, None, None, None, None, 'The fence is loose.')
+b_fence = Room('Broken Fence', 'Front Gate', None, None, 'Sewer Lid')
