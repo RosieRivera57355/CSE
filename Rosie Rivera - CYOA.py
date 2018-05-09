5ba6f668d9e3d4af20272c1249d4e5dafcf1f168
Rosie Rivera <33767543+RosieRivera57355@users.noreply.github.com>
2018-05-09 15:38:21 -0700

    Added Stuff

--- Rosie Rivera - CYOA.py
+++ Rosie Rivera - CYOA.py
@@ -223,31 +223,32 @@ b_fence = Room('Broken Fence', 'f_gate', None, None, None, None, None, None, Non
                'The fence is loose.', 'Fence', None, None)
 garden = Room('Garden', 's_house', None, None, None, None, 'shed', None, None, 'b_fence',
               None, 'There is a path leading Northeast somewhere and there is a broken fence south of here.')
-s_house = Room('South of House', 'l_room', 'garden', 'w_house', 'East of House', None, None, None, None,
+s_house = Room('South of House', 'l_room', 'garden', 'w_house', 'e_house', None, None, None, None,
                None, None, 'There is a window that is partly open. There seems to be something shining inside')
-l_room = Room('Living Room', None, 'South of House', None, None, None, None, None, None, 'Up Stairs',
-              'Down Stairs', 'There seems to be an upstairs and downstairs and not much else...You should look around.')
-k_room = Room('Kitchen', None, None, 'Living Room', None, None, None, None, None, None, None,
+l_room = Room('Living Room', None, 's_house', None, None, None, None, None, None, 'u_stairs',
+              'd_stairs', 'There seems to be an upstairs and downstairs and not much else...You should look around.')
+k_room = Room('Kitchen', None, None, 'l_room', None, None, None, None, None, None, None,
               'There is a fridge, an island, and cupboards.'
               '\n There is a knife on a cutting board, a sink, a coffee machine, coffee grounds, and a mug')
-up_stairs = Room('Up Stairs', None, None, 'Justin Timberlakes Room', 'Guest Room', None, None, None, None,
-                 None, 'Living Room', 'There is a room WEST of here and a room EAST of here.'
-                                      'There is a mirror and a vase that has some flowers')
-jt_room = Room('Justin Timberlakes', None, None, None, 'Up Stairs', None, None, None, None, None, None,
+up_stairs = Room('Up Stairs', None, None, 'jt_room', 'g_room', None, None, None, None,
+                 None, 'l_room', 'There is a room WEST \n'
+                                 'of here and a room EAST of here. There is a mirror and a vase that has some flowers')
+jt_room = Room('Justin Timberlakes Room', None, None, None, 'u_stairs', None, None, None, None, None, None,
                'There is a phone on the bedside table. There is also a TV, bed, and a drawer.')
-parking_lot = Room('Parking Lot', 'Garage', 'Living Room', None, None, None, None, None, None, None, None,
+parking_lot = Room('Parking Lot', 'garage', 'l_room', None, None, None, None, None, None, None, None,
                    'There is a garage that is open. Inside, there is are 3 boxes with stuff inside.')
-d_house = Room('Dog House', 'Dogs Room', 'Back Yard', None, None, None, None, None, None, None, None,
+d_house = Room('Dog House', 'd_room', 'y_back', None, None, None, None, None, None, None, None,
                'There is a big dog house and from outside you can hear a big dog barking. Will you go inside?')
-d_room = Room('Dog Room', None, 'Dog House', None, None, None, None, None, None, None, None,
+d_room = Room('Dog Room', None, 'd_house', None, None, None, None, None, None, None, None,
               'There is a small dog in here and it seems to want something. You cannot get '
               'close enough to it to see what is on its collar.')
-y_back = Room('Back Yard', None, 'Shed', 'Kitchen', None, None, None, None, None, None, None,
+y_back = Room('Back Yard', None, 'shed', 'k_room', None, None, None, None, None, None, None,
               'There is a shed south of here. There could be something useful in there.')
-shed = Room('Shed', 'Back Yard', 'Garden', None, None, None, None, None, None, None, None,
+shed = Room('Shed', 'y_back', 'garden', None, None, None, None, None, None, None, None,
             'There are two paths one goes Southwest and one goes north. Inside there is a picture.')
-d_stairs = Room('Down Stairs', None, None, None, None, None, None, None, None, 'Living Room', None,
+d_stairs = Room('Down Stairs', None, None, None, None, None, None, None, None, 'l_room', None,
                 'It is dark and there is a room with a whole bunch of junk.')
+g_room = Room('Guest Room', 'w_house', None, None, None, None, 'l_room', None, 's_house', None, None, 'There is')
 current_node = f_gate
 
 while True:
